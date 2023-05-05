995b540ab0088d4eff0a74a8a05ab540bf083472
D. Vinevcev <dwinner@inbox.ru>
2023-05-05 20:29:57 +0300

    books updated

--- Algs/code/Java. Algs/4-Graphs/03-DepthFirstPaths.java
+++ Algs/code/Java. Algs/4-Graphs/03-DepthFirstPaths.java
@@ -1,20 +1,22 @@
-/*************************************************************************
+/******************************************************************************
  *  Compilation:  javac DepthFirstPaths.java
  *  Execution:    java DepthFirstPaths G s
  *  Dependencies: Graph.java Stack.java StdOut.java
- *  Data files:   http://algs4.cs.princeton.edu/41undirected/tinyCG.txt
+ *  Data files:   https://algs4.cs.princeton.edu/41graph/tinyCG.txt
+ *                https://algs4.cs.princeton.edu/41graph/tinyG.txt
+ *                https://algs4.cs.princeton.edu/41graph/mediumG.txt
+ *                https://algs4.cs.princeton.edu/41graph/largeG.txt
  *
- *  Run depth first search on an undirected graph.
- *  Runs in O(E + V) time.
+ *  Run depth-first search on an undirected graph.
  *
  *  %  java Graph tinyCG.txt
  *  6 8
- *  0: 2 1 5 
- *  1: 0 2 
- *  2: 0 1 3 4 
- *  3: 5 4 2 
- *  4: 3 2 
- *  5: 3 0 
+ *  0: 2 1 5
+ *  1: 0 2
+ *  2: 0 1 3 4
+ *  3: 5 4 2
+ *  4: 3 2
+ *  5: 3 0
  *
  *  % java DepthFirstPaths tinyCG.txt 0
  *  0 to 0:  0
@@ -24,46 +26,16 @@
  *  0 to 4:  0-2-3-4
  *  0 to 5:  0-2-3-5
  *
- *************************************************************************/
+ ******************************************************************************/
 
-public class DepthFirstPaths {
-    private boolean[] marked;    // marked[v] = is there an s-v path?
-    private int[] edgeTo;        // edgeTo[v] = last edge on s-v path
-    private final int s;         // source vertex
+package edu.princeton.cs.algs4;
 
-    public DepthFirstPaths(Graph G, int s) {
-        this.s = s;
-        edgeTo = new int[G.V()];
-        marked = new boolean[G.V()];
-        dfs(G, s);
-    }
-
-    // depth first search from v
-    private void dfs(Graph G, int v) {
-        marked[v] = true;
-        for (int w : G.adj(v)) {
-            if (!marked[w]) {
-                edgeTo[w] = v;
-                dfs(G, w);
-            }
-        }
-    }
-
-    // is there a path between s and v?
-    public boolean hasPathTo(int v) {
-        return marked[v];
-    }
-
-    // return a path between s to v; null if no such path
-    public Iterable<Integer> pathTo(int v) {
-        if (!hasPathTo(v)) return null;
-        Stack<Integer> path = new Stack<Integer>();
-        for (int x = v; x != s; x = edgeTo[x])
-            path.push(x);
-        path.push(s);
-        return path;
-    }
-
+public class DepthFirstPaths {    
+    /**
+     * Unit tests the {@code DepthFirstPaths} data type.
+     *
+     * @param args the command-line arguments
+     */
     public static void main(String[] args) {
         In in = new In(args[0]);
         Graph G = new Graph(in);
