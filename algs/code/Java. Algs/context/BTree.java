b9070d4833928d5fd8e83421480840e8e678925f
Denis Vinevcev <dwinner@inbox.ru>
2023-08-16 22:47:46 +0300

    misc

--- algs/code/Java. Algs/context/BTree.java
+++ algs/code/Java. Algs/context/BTree.java
@@ -1,44 +1,5 @@
-/******************************************************************************
- *  Compilation:  javac BTree.java
- *  Execution:    java BTree
- *  Dependencies: StdOut.java
- *
- *  B-tree.
- *
- *  Limitations
- *  -----------
- *   -  Assumes M is even and M >= 4
- *   -  should b be an array of children or list (it would help with casting to make it a list)
- *
- ******************************************************************************/
-
-package edu.princeton.cs.algs4;
-
-/**
- *  The {@code BTree} class represents an ordered symbol table of generic
- *  key-value pairs.
- *  It supports the <em>put</em>, <em>get</em>, <em>contains</em>,
- *  <em>size</em>, and <em>is-empty</em> methods.
- *  A symbol table implements the <em>associative array</em> abstraction:
- *  when associating a value with a key that is already in the symbol table,
- *  the convention is to replace the old value with the new value.
- *  Unlike {@link java.util.Map}, this class uses the convention that
- *  values cannot be {@code null}—setting the
- *  value associated with a key to {@code null} is equivalent to deleting the key
- *  from the symbol table.
- *  <p>
- *  This implementation uses a B-tree. It requires that
- *  the key type implements the {@code Comparable} interface and calls the
- *  {@code compareTo()} and method to compare two keys. It does not call either
- *  {@code equals()} or {@code hashCode()}.
- *  The <em>get</em>, <em>put</em>, and <em>contains</em> operations
- *  each make log<sub><em>m</em></sub>(<em>n</em>) probes in the worst case,
- *  where <em>n</em> is the number of key-value pairs
- *  and <em>m</em> is the branching factor.
- *  The <em>size</em>, and <em>is-empty</em> operations take constant time.
- *  Construction takes constant time.
- */
-public class BTree<Key extends Comparable<Key>, Value>  {
+public class BTree<Key extends Comparable<Key>, Value>  
+{
     // max children per B-tree node = M-1
     // (must be even and greater than 2)
     private static final int M = 4;
@@ -137,7 +98,6 @@ public class BTree<Key extends Comparable<Key>, Value>  {
         return null;
     }
 
-
     /**
      * Inserts the key-value pair into the symbol table, overwriting the old value
      * with the new value if the key is already in the symbol table.
@@ -230,7 +190,6 @@ public class BTree<Key extends Comparable<Key>, Value>  {
         return s.toString();
     }
 
-
     // comparison functions - make Comparable instead of Key to avoid casts
     private boolean less(Comparable k1, Comparable k2) {
         return k1.compareTo(k2) < 0;
@@ -238,47 +197,5 @@ public class BTree<Key extends Comparable<Key>, Value>  {
 
     private boolean eq(Comparable k1, Comparable k2) {
         return k1.compareTo(k2) == 0;
-    }
-
-
-    /**
-     * Unit tests the {@code BTree} data type.
-     *
-     * @param args the command-line arguments
-     */
-    public static void main(String[] args) {
-        BTree<String, String> st = new BTree<String, String>();
-
-        st.put("www.cs.princeton.edu", "128.112.136.12");
-        st.put("www.cs.princeton.edu", "128.112.136.11");
-        st.put("www.princeton.edu",    "128.112.128.15");
-        st.put("www.yale.edu",         "130.132.143.21");
-        st.put("www.simpsons.com",     "209.052.165.60");
-        st.put("www.apple.com",        "17.112.152.32");
-        st.put("www.amazon.com",       "207.171.182.16");
-        st.put("www.ebay.com",         "66.135.192.87");
-        st.put("www.cnn.com",          "64.236.16.20");
-        st.put("www.google.com",       "216.239.41.99");
-        st.put("www.nytimes.com",      "199.239.136.200");
-        st.put("www.microsoft.com",    "207.126.99.140");
-        st.put("www.dell.com",         "143.166.224.230");
-        st.put("www.slashdot.org",     "66.35.250.151");
-        st.put("www.espn.com",         "199.181.135.201");
-        st.put("www.weather.com",      "63.111.66.11");
-        st.put("www.yahoo.com",        "216.109.118.65");
-
-
-        StdOut.println("cs.princeton.edu:  " + st.get("www.cs.princeton.edu"));
-        StdOut.println("hardvardsucks.com: " + st.get("www.harvardsucks.com"));
-        StdOut.println("simpsons.com:      " + st.get("www.simpsons.com"));
-        StdOut.println("apple.com:         " + st.get("www.apple.com"));
-        StdOut.println("ebay.com:          " + st.get("www.ebay.com"));
-        StdOut.println("dell.com:          " + st.get("www.dell.com"));
-        StdOut.println();
-
-        StdOut.println("size:    " + st.size());
-        StdOut.println("height:  " + st.height());
-        StdOut.println(st);
-        StdOut.println();
     }
 }
